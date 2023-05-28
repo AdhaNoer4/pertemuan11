028280c70679021c6ee7802b05686e9287ff8758
AdhaNoer4 <adhanoerhidayah4@gmail.com>
2023-05-28 16:58:15 +0700

    Membuat Live search dengan AJAX

--- index.php
+++ index.php
@@ -29,38 +29,39 @@ if(isset($_POST['cari'])){
    
     <a href="tambah.php">Tambah</a><br><br>
     <form action="" method="post">
-        <input type="text" name="keyword" size="40" autofocus placeholder="Masukan Keyword..." autocomplete="off">
-        <button type="submit" name="cari" >Cari</button>
+        <input type="text" name="keyword" size="40" autofocus placeholder="Masukan Keyword..." autocomplete="off" id="keyword">
+        <button type="submit" name="cari" id="tombol-cari" >Cari</button>
         </form>
-    <table border="1" cellpadding="10" cellspacing="0">
-        <tr>
-            <th>No.</th>
-            <th>Aksi</th>
-            <th>gambar</th>
-            <th>NRP</th>
-            <th>Nama</th>
-            <th>Email</th>
-            <th>Jurusan</th>
-        </tr>
-<?php $i = 1; ?>
-        <?php foreach($mahasiswa as $mhs) : ?>
-           
-        <tr>
-            <td><?= $i; ?></td>
-            <td>
-                <a href="ubah.php?id=<?= $mhs['id'] ?>">Edit</a> 
-                <a href="hapus.php?id=<?= $mhs['id'] ?>" onclick="return confirm('Apakah anda Yakin?')">Delete</a>
-            </td>
-            <td><img src="img/<?= $mhs['gambar'] ?>" width="50" alt=""></td>
-            <td><?= $mhs['nrp']; ?></td>
-            <td><?= $mhs['nama']; ?></td>
-            <td><?= $mhs['email']; ?></td>
-            <td><?= $mhs['jurusan']; ?></td>
-        </tr>
-        <?php $i++ ?>
-        <?php endforeach; ?>
-    </table>
-
-
+        <div id="container">
+                <table border="1" cellpadding="10" cellspacing="0">
+                    <tr>
+                        <th>No.</th>
+                        <th>Aksi</th>
+                        <th>gambar</th>
+                        <th>NRP</th>
+                        <th>Nama</th>
+                        <th>Email</th>
+                        <th>Jurusan</th>
+                    </tr>
+            <?php $i = 1; ?>
+                    <?php foreach($mahasiswa as $mhs) : ?>
+                    
+                    <tr>
+                        <td><?= $i; ?></td>
+                        <td>
+                            <a href="ubah.php?id=<?= $mhs['id'] ?>">Edit</a> 
+                            <a href="hapus.php?id=<?= $mhs['id'] ?>" onclick="return confirm('Apakah anda Yakin?')">Delete</a>
+                        </td>
+                        <td><img src="img/<?= $mhs['gambar'] ?>" width="50" alt=""></td>
+                        <td><?= $mhs['nrp']; ?></td>
+                        <td><?= $mhs['nama']; ?></td>
+                        <td><?= $mhs['email']; ?></td>
+                        <td><?= $mhs['jurusan']; ?></td>
+                    </tr>
+                    <?php $i++ ?>
+                    <?php endforeach; ?>
+                </table>
+    </div>
+<script src="js/script.js"></script>
 </body>
 </html>
